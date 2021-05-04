f829a2cb05746d3bb7bd948758a3a7f8ac5e9755
Stack_Pointer <kiranchamp.6457@gmail.com>
2021-05-04 20:38:39 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState(false);
+
+  const paraTag = () => {
+    setPara(true);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      {para ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        <button id="click" onClick={paraTag}>
+          Click!
+        </button>
+      )}
     </div>
   );
 }
-
 
 export default App;
